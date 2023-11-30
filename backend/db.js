fae0fafaeeaf5f2cd0c401ebb92713e6bc9dc54c
Akshay Salunkhe <ak.salunkhe7777@gmail.com>
2023-11-30 22:39:08 +0530

    Add

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,15 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+
+const DB_URI = "mongodb+srv://ganeshjagrut111:XuBsPja0ye9tJi9E@cluster0.dl6dihd.mongodb.net/back?retryWrites=true&w=majority";
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/backend");
 
-module.exports=mongoose;
+
+
+
+module.exports = mongoose;
